5ecf56166cb41996af758195c54839eb70af6b87
willwinworld <zhangzhouit@163.com>
2018-04-20 15:32:16 +0800

    adding comments to the post detail template

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,12 +1,19 @@
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    comment = forms.CharField(required=False, widget=forms.Textarea)
+    comments = forms.CharField(required=False, widget=forms.Textarea)
     # Each field type has a default widget that determines how the fields is displayed in HTML
     # The default widget can be overridden with the widget attribute
     # In the comments field, we use a Textarea widget to display it as a <textarea>
     #  HTML element inside of the default <input> element.
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
