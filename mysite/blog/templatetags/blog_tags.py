645757adbb3099f93854c541364b4e7aae7f31c3
willwinworld <zhangzhouit@163.com>
2018-05-02 23:53:44 +0800

    adding a sitemap to your site

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -4,6 +4,13 @@ register = template.Library()
 
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 @register.simple_tag
